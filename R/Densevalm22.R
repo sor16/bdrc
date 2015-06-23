ffc7f7215e4e990489c4df0c3b42a95a44076912
sor16 <solviro@gmail.com>
2015-06-23 15:02:48 +0000

    new functions added to RCmodels

---
+++ R/Densevalm22.R
@@ -0,0 +1,35 @@
+Densevalm22 <- function(th,RC){
+  phi_b=th[3]
+  sig_b2=th[2]
+  zeta=th[1]
+  lambda=th[4:9]
+  
+  f=lambda[1:5]-lambda[6]
+  l=c(log(RC$w_tild+exp(th[1])))
+  
+  varr=c(exp(RC$B%*%lambda))
+  Sig_eps=diag(c(varr,0))
+  R_Beta=(1+sqrt(5)*RC$dist/exp(phi_b)+5*RC$dist^2/(3*exp(phi_b)^2))*exp(-sqrt(5)*RC$dist/exp(phi_b))+diag(RC$n)*RC$nugget
+  Sig_x=rbind(cbind(RC$Sig_ab,RC$m1),cbind(RC$m2,exp(sig_b2)*R_Beta))
+  
+  X=rbind(cbind(1,l,diag(l)%*%RC$A),RC$Z)
+  L=t(chol(X%*%Sig_x%*%t(X)+Sig_eps))
+  w=solve(L,RC$y-X%*%RC$mu_x)
+  p=as.numeric(-0.5%*%t(w)%*%w-sum(log(diag(L)))-
+                 (RC$v+5-1)/2*log(RC$v*RC$s+f%*%RC$P%*%f)+
+                 sig_b2-exp(sig_b2)/RC$mu_sb+zeta-exp(zeta)/RC$mu_c-0.5/RC$tau_pb2*(phi_b-RC$mu_pb)^2) 
+  
+  W=solve(L,X%*%Sig_x)
+  x_u=RC$mu_x+t(chol(Sig_x))%*%rnorm(RC$n+2)
+  sss=(X%*%x_u)-RC$y+rbind(sqrt(varr)*as.matrix(rnorm(RC$N)),0)
+  x=as.matrix(x_u-t(W)%*%solve(L,sss))
+  yp=X %*% x 
+  yp=yp[1:RC$N,]
+  ypo=yp+as.matrix(rnorm(RC$n))*sqrt(varr)
+  
+  D=-2*sum(log(dlnorm(exp(RC$y[1:RC$N,]),yp,sqrt(varr))))
+  
+  return(list("p"=p,"x"=x,"yp"=yp,"ypo"=ypo,"D"=D,"varr"=varr))
+
+  
+}
---
+++ R/model2.R
@@ -0,0 +1,133 @@
+#Model2BH
+library(stats)
+model1<-function(wq,RC){
+    Nit=20000;
+
+    RC$mu_a=3.20
+    RC$mu_b=2.29
+    RC$sig_a=sqrt(1.21)
+    RC$sig_b=sqrt(0.48)
+    RC$p_ab=-0.61
+    RC$mu_c=1.9000
+    RC$nugget=10^-8
+    RC$mu_sb=0.5
+    RC$mu_pb=0.5
+    RC$tau_pb2=0.25^2
+    RC$s=3
+    RC$v=5
+    RC$y=rbind(as.matrix(log(wq[,2])),0)
+    RC$w=as.matrix(0.01*wq[,1])
+    RC$w_tild=RC$w-min(RC$w)
+
+    Adist1 <- Adist(RC$w)
+    RC$A=Adist1$A
+    RC$dist=Adist1$dist
+    RC$n=Adist1$n
+    RC$N=Adist1$N
+
+    RC$P=diag(nrow=5,ncol=5,6)-matrix(nrow=5,ncol=5,1)
+    RC$Sig_ab= rbind(c(RC$sig_a^2, RC$p_ab*RC$sig_a*RC$sig_b), c(RC$p_ab*RC$sig_a*RC$sig_b, RC$sig_b^2))
+    RC$mu_x=as.matrix(c(RC$mu_a,RC$mu_b, rep(0,RC$n))) #Setja i RC
+
+    RC$B=B_splines(t(RC$w_tild)/RC$w_tild[length(RC$w_tild)])
+    RC$Z=cbind(t(rep(0,2)),t(rep(1,RC$n)))
+    RC$m1=matrix(0,nrow=2,ncol=RC$n)
+    RC$m2=matrix(0,nrow=RC$n,ncol=2)
+
+    Dens = function(th) {-Densevalm22fast(th,RC)$p}
+
+    Densmin=optim(par=theta.init,Dens,method="BFGS",hessian=TRUE)
+
+    t_m =Densmin$par
+    H=Densmin$hessian
+
+    t1=matrix(0,9,Nit)
+    t2=matrix(0,9,Nit)
+    t3=matrix(0,9,Nit)
+    t4=matrix(0,9,Nit)
+
+
+    xsiz=max(dim(mu));
+    x1=matrix(0,xsiz,Nit)
+    x2=matrix(0,xsiz,Nit)
+    x3=matrix(0,xsiz,Nit)
+    x4=matrix(0,xsiz,Nit)
+
+
+    for(j in 1:4){
+      t_old=t_m
+      t=matrix(0,nrow=9,ncol=Nit)
+      x=matrix(0,nrow=xsiz,ncol=Nit)
+      yp=matrix(0,nrow=RC$N,ncol=Nit)
+      ypo=matrix(0,nrow=RC$N,ncol=Nit)
+      varr=matrix(0,nrow=RC$N,ncol=Nit)
+      D=matrix(0,nrow=1,ncol=Nit)
+
+
+
+      Dens<-Densevalm22fast(t_old,RC)
+      p_old=Dens$p
+      x_old=Dens$x
+      yp_old=Dens$yp
+      ypo_old=Dens$ypo
+      D_old=Dens$D
+      varr_old=Dens$varr
+
+      for(i in 1:Nit){
+        t_new=t_old+solve(t(LH),as.matrix(rnorm(9,0,1)))
+
+        Densnew<-Densevalm22fast(t_new,RC)
+        p_new=Densnew$p
+        x_new=Densnew$x
+        yp_new=Densnew$yp
+        ypo_new=Densnew$ypo
+        D_new=Densnew$D
+        varr_new=Densnew$varr
+
+        logR=p_new-p_old
+
+        if (logR>log(runif(1))){
+          t_old=t_new
+          x_old=x_new
+          p_old=p_new
+          yp_old=yp_new
+          ypo_old=ypo_new
+          D_old=D_new
+          varr_old=varr_new
+        }
+
+        t[,i]=t_old
+        yp[,i]=yp_old
+        ypo[,i]=ypo_old
+
+        D[1,i]=D_old
+        varr[,i]=varr_old
+      }
+
+      if(j==1){
+        t1=t
+        yp1=yp
+        ypo1=ypo
+        D1=D
+        varr1=varr
+      } else if(j==2){
+        t2=t
+        yp2=yp
+        ypo2=ypo
+        D2=D
+        varr2=varr
+      } else if(j==3){
+        t3=t
+        yp3=yp
+        ypo3=ypo
+        D3=D
+        varr3=varr
+      } else if(j==4){
+        t4=t
+        yp4=yp
+        ypo4=ypo
+        D4=D
+        varr4=varr
+      }
+    }
+}
