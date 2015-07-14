ceef917988a5ee6cb5c7df28da4f475d9eff99c6
sor16 <solviro@gmail.com>
2015-07-14 16:16:27 +0000

    fixing if null stuff and rounding

--- R/W_unobserved.R
+++ R/W_unobserved.R
@@ -1,4 +1,6 @@
 W_unobserved <- function(W_unique,min=NULL,max=NULL){
+    W_u=NULL
+    W_u_tild=NULL
     #distance between subsequent elements in vector with additional dummy point 1000
     min=round(min,2)
     max=round(max,2)
@@ -7,14 +9,17 @@ W_unobserved <- function(W_unique,min=NULL,max=NULL){
     #add datapoints to corresponding distances to see range of distance
     distwithdata=rbind(w,distvect,c(w[2:length(w)],1000))
     distfilter=distwithdata[,distvect>0.04]
-    #remove dummy distance
-    distfilter=distfilter[,-ncol(distfilter)]
-    #make sequence from the ranges with length.out equal to corresponding elelement in distvect
-    W_u=0.01*unlist(apply(distfilter,2,FUN=function(x){setdiff(seq(x[1],x[3],length.out=round(x[2])),c(x[1],x[3]))
-    }))
+
+    if(!is.numeric(distfilter)){
+        #remove dummy distance
+        distfilter=distfilter[,-ncol(distfilter)]
+        #make sequence from the ranges with length.out equal to corresponding elelement in distvect
+        W_u=0.01*unlist(apply(distfilter,2,FUN=function(x){setdiff(seq(x[1],x[3],length.out=round(x[2])),c(x[1],x[3]))
+        }))
+    }
     if(!is.null(min)|!is.null(max)){
         minseq=setdiff(seq(min,min(W_unique),by=0.01),c(min(W_unique)))
-        maxseq=setdiff(seq(round(max(W_unique)),max,by=0.01),c(round(max(W_unique))))
+        maxseq=setdiff(seq(round(max(W_unique),2),max,by=0.01),c(round(max(W_unique),2)))
         W_spline=c(rep(min(W_unique),length(minseq)),W_u,rep(max(W_unique),length(maxseq)))
         W_u=c(minseq,W_u,maxseq)
         W_u_tild=W_spline-min(W_unique)
