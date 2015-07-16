50eef4d6272ce6d0552f32a4788ae0db14ca9661
sor16 <solviro@gmail.com>
2015-07-16 10:56:34 +0000

    test W_unobserved, little bit further appart from min and to max

--- R/W_unobserved.R
+++ R/W_unobserved.R
@@ -18,8 +18,8 @@ W_unobserved <- function(W_unique,min=NULL,max=NULL){
         }))
     }
     if(!is.null(min)|!is.null(max)){
-        minseq=setdiff(seq(min,min(W_unique),by=0.01),c(min(W_unique)))
-        maxseq=setdiff(seq(round(max(W_unique),2),max,by=0.01),c(round(max(W_unique),2)))
+        minseq=setdiff(seq(min,min(W_unique),by=0.02),c(min(W_unique)))
+        maxseq=setdiff(seq(round(max(W_unique),2),max,by=0.02),c(round(max(W_unique),2)))
         W_spline=c(rep(min(W_unique),length(minseq)),W_u,rep(max(W_unique),length(maxseq)))
         W_u=c(minseq,W_u,maxseq)
         W_u_tild=W_spline-min(W_unique)
