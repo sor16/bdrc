f3d41503204009813eeb63897e16f36e3b4b46d3
sor16 <solviro@gmail.com>
2015-07-22 11:19:02 +0000

    is.null again

--- R/clean.R
+++ R/clean.R
@@ -13,7 +13,7 @@ clean <- function(file,advanced=FALSE,slider=0,dummy=NULL,keeprows=NULL){
         qvdata$Quality=gsub('\\s+', '',qvdata$Quality)
         qvdata$W=0.01*qvdata$W
         qvdata=qvdata[with(qvdata,order(W)),]
-        if(!is.null(length(keeprows))){
+        if(length(keeprows)!=0){
             qvdata=qvdata[keeprows,]
         }
         if(advanced==TRUE){
