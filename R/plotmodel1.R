904644b8c7898849b57fa7e69ec9a3f5fdf518b0
axelornj <aoj8@hi.is>
2015-08-12 14:45:25 +0000

    plots for model 1 and 2

---
+++ R/plotmodel1.R
@@ -0,0 +1,70 @@
+
+plotmodel1 <- function(filename,eps=F,png=F,realscale=T,logscale=T,logresiduals=T,realresiduals=T){
+
+    library(ggplot2)
+    if (is.null(filename)){
+        return(NULL)
+    }
+
+    require(RCmodels)
+    list2env(clean,environment())
+
+    data <- model1BH(clean(filename))
+    simdata=data$simdata
+    realdata=data$realdata
+
+    plotlist=list()
+
+    if(realscale==TRUE){
+        rcreal=ggplot(simdata)+theme_bw()+geom_point(data=realdata,aes(exp(Q),W))+geom_line(aes(exp(fit),W))+
+               geom_line(aes(exp(lower),W),linetype="dashed")+geom_line(aes(exp(upper),W),linetype="dashed")+
+               ggtitle(paste("Rating curve for",gsub("\\.[^.]*$","",filename)))+ylab("W  [m]")+xlab(expression(paste("Q  [",m^3,'/s]',sep='')))+
+               theme(plot.title = element_text(vjust=2))
+
+
+        plotlist$rcreal=rcreal
+    }
+    if(logscale==TRUE){
+        rclog=ggplot(realdata)+geom_line(mapping=aes(fit,l_m))+theme_bw()+geom_point(mapping=aes(Q,l_m))+geom_line(mapping=aes(lower,l_m),linetype="dashed")+
+              geom_line(mapping=aes(upper,l_m),linetype="dashed")+ggtitle(paste("Rating curve for",gsub("\\.[^.]*$","",filename),"(log scale)"))+
+              ylab(expression(log(W-hat(c))))+xlab("log(Q)")+theme(plot.title = element_text(vjust=2))
+
+        plotlist$rclog=rclog
+    }
+    if(logresiduals==TRUE){
+        rcrealresid=ggplot(realdata)+geom_point(aes(W,residraun),color="red")+theme_bw()+geom_abline(intercept = 0, slope = 0)+
+            geom_line(aes(W,residupper),linetype="dashed")+geom_line(aes(W,residlower),linetype="dashed")+ylab(expression(paste("Q - ",hat(Q) ,"  [",m^3,'/s]',sep='')))+
+            ggtitle("Residual plot")+xlab("W  [cm]")+theme(plot.title = element_text(vjust=2))
+
+        plotlist$rcrealresid=rcrealresid
+    }
+    if(realresiduals==TRUE){
+        rclogresid=ggplot(realdata)+geom_point(aes(l_m,residlog),color="red")+theme_bw()+geom_abline(intercept = 0, slope = 0)+
+            geom_abline(intercept = 2, slope = 0,linetype="dashed")+geom_abline(intercept = -2, slope = 0,linetype="dashed")+
+            ylab(expression(epsilon[i]))+ggtitle("Residual plot (log scale)")+xlab(expression(log(W-hat(c))))+
+            theme(plot.title = element_text(vjust=2))
+
+
+        plotlist$rclogresid=rclogresid
+    }
+    if(png==TRUE){
+        if(length(plotlist)!=0){
+            for(i in 1:length(plotlist)){
+            png(file = paste(gsub("\\.[^.]*$","",filename),'_',i,'_','model1','.png', sep=""))
+            print(plotlist[i])
+            dev.off()
+            }
+        }
+    }
+    if(eps==TRUE){
+        if(length(plotlist)!=0){
+            for(i in 1:length(plotlist)){
+            postscript(file = paste(gsub("\\.[^.]*$","",filename),'_',i,'_','model1','.eps', sep=""), width = 10, height = 7.5)
+            print(plotlist[i])
+            dev.off()
+            }
+        }
+    }
+
+    return(plotlist)
+}
