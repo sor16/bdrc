ec9c23609d29da7c1b491a1e224fa9e6baebba8b
sor16 <solviro@gmail.com>
2015-08-13 14:59:36 +0000

    W unobserved, filter=5cm

--- R/W_unobserved.R
+++ R/W_unobserved.R
@@ -5,21 +5,22 @@ W_unobserved <- function(W_unique,min=NULL,max=NULL){
     min=round(min,2)
     max=round(max,2)
     w=100*W_unique #work in cm
+    filter=5
     distvect=abs(w-c(w[2:length(w)],1000))
     #add datapoints to corresponding distances to see range of distance
     distwithdata=rbind(w,distvect,c(w[2:length(w)],1000))
-    distfilter=distwithdata[,distvect>4]
+    distfilter=distwithdata[,distvect>filter]
 
     if(ncol(distfilter)!=0){
         #remove dummy distance
         distfilter=distfilter[,-ncol(distfilter)]
         #make sequence from the ranges with length.out equal to corresponding elelement in distvect
-        W_u=0.01*unlist(apply(distfilter,2,FUN=function(x){setdiff(seq(x[1],x[3],length.out=round(x[2])),c(x[1],x[3]))
+        W_u=0.01*unlist(apply(distfilter,2,FUN=function(x){setdiff(seq(x[1],x[3],length.out=ceiling(x[2]/filter)),c(x[1],x[3]))
         }))
     }
     if(!is.null(min)|!is.null(max)){
-        minseq=setdiff(seq(min,min(W_unique),by=0.02),c(min(W_unique)))
-        maxseq=setdiff(seq(round(max(W_unique),2),max,by=0.02),c(round(max(W_unique),2)))
+        minseq=setdiff(seq(min,min(W_unique),by=0.05),c(min(W_unique)))
+        maxseq=setdiff(seq(round(max(W_unique),2),max,by=0.05),c(round(max(W_unique),2)))
         W_spline=c(rep(min(W_unique),length(minseq)),W_u,rep(max(W_unique),length(maxseq)))
         W_u=c(minseq,W_u,maxseq)
         W_u_tild=W_spline-min(W_unique)
