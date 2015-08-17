2848825eb74432a7a7437eccfbd61b426fcef090
sor16 <solviro@gmail.com>
2015-08-17 10:45:13 +0000

    Bsplines all one function now

--- R/B_splines.R
+++ R/B_splines.R
@@ -34,7 +34,62 @@ lx = length(ZZ)
 
 #Compute the x-splines and the y-splines.
 #[XX] = spline_functions(ZZ,tau_x,dx,kx,M);
-XX <- spline_functions(ZZ,tau_x,dx,kx,M)
+
+    # function [XX] = spline_functions(ZZ,tau_x,dx,kx,M)
+    #
+    # XX = zeros(kx+M,length(ZZ));
+    XX = matrix(0,nrow=(kx+M),ncol=length(ZZ))
+    #
+    # % i = 1
+    XX[1,] = (1/dx^3)*(tau_x[M+1]-ZZ)*(tau_x[M+1]-ZZ)*(tau_x[M+1]-ZZ)*(tau_x[M]<=ZZ)*(ZZ<tau_x[M+1]);
+
+    # % i = 2
+    XX[2,] = (1/dx^3)*(ZZ-tau_x[2])*(tau_x[M+1]-ZZ)*(tau_x[M+1]-ZZ)*(tau_x[M]<=ZZ)*(ZZ<tau_x[M+1])+
+        (1/2/dx^3)*(tau_x[M+2]-ZZ)*(ZZ-tau_x[3])*(tau_x[M+1]-ZZ)*(tau_x[M]<=ZZ)*(ZZ<tau_x[M+1])+
+        (1/4/dx^3)*(tau_x[M+2]-ZZ)*(tau_x[M+2]-ZZ)*(ZZ-tau_x[M])*(tau_x[M]<=ZZ)*(ZZ<tau_x[M+1])+
+        (1/4/dx^3)*(tau_x[M+2]-ZZ)*(tau_x[M+2]-ZZ)*(tau_x[M+2]-ZZ)*(tau_x[M+1]<=ZZ)*(ZZ<tau_x[M+2])
+
+    # % i = 3
+    XX[3,] = (1/2/dx^3)*(ZZ-tau_x[3])*(ZZ-tau_x[3])*(tau_x[M+1]-ZZ)*(tau_x[M]<=ZZ)*(ZZ<tau_x[M+1])+
+        (1/4/dx^3)*(ZZ-tau_x[3])*(tau_x[M+2]-ZZ)*(ZZ-tau_x[M])*(tau_x[M]<=ZZ)*(ZZ<tau_x[M+1])+
+        (1/4/dx^3)*(ZZ-tau_x[3])*(tau_x[M+2]-ZZ)*(tau_x[M+2]-ZZ)*(tau_x[M+1]<=ZZ)*(ZZ<tau_x[M+2])+
+        (1/6/dx^3)*(tau_x[M+3]-ZZ)*(ZZ-tau_x[M])*(ZZ-tau_x[M])*(tau_x[M]<=ZZ)*(ZZ<tau_x[M+1])+
+        (1/6/dx^3)*(tau_x[M+3]-ZZ)*(ZZ-tau_x[M])*(tau_x[M+2]-ZZ)*(tau_x[M+1]<=ZZ)*(ZZ<tau_x[M+2])+
+        (1/6/dx^3)*(tau_x[M+3]-ZZ)*(tau_x[M+3]-ZZ)*(ZZ-tau_x[M+1])*(tau_x[M+1]<=ZZ)*(ZZ<tau_x[M+2])+
+        (1/6/dx^3)*(tau_x[M+3]-ZZ)*(tau_x[M+3]-ZZ)*(tau_x[M+3]-ZZ)*(tau_x[M+2]<=ZZ)*(ZZ<tau_x[M+3])
+
+    # % i = 4,...,kx + 1
+    #   for (kk in M:(kx + 1)){
+    #     XX[kk,] = (1/6/dx^3)*(ZZ-tau_x[kk])*(ZZ-tau_x[kk])*(ZZ-tau_x[kk])*(tau_x[kk]<=ZZ)*(ZZ<tau_x[kk+1])+
+    #       (1/6/dx^3)*(ZZ-tau_x[kk])*(ZZ-tau_x[kk])*(tau_x[kk+2]-ZZ)*(tau_x[kk+1]<=ZZ)*(ZZ<tau_x[kk+2])+
+    #       (1/6/dx^3)*(ZZ-tau_x[kk])*(tau_x[kk+3]-ZZ)*(ZZ-tau_x[kk+1])*(tau_x[kk+1]<=ZZ)*(ZZ<tau_x[kk+2])+
+    #       (1/6/dx^3)*(ZZ-tau_x[kk])*(tau_x[kk+3]-ZZ)*(tau_x[kk+3]-ZZ)*(tau_x[kk+2]<=ZZ)*(ZZ<tau_x[kk+3])+
+    #       (1/6/dx^3)*(tau_x[kk+4]-ZZ)*(ZZ-tau_x[kk+1])*(ZZ-tau_x[kk+1])*(tau_x[kk+1]<=ZZ)*(ZZ<tau_x[kk+2])+
+    #       (1/6/dx^3)*(tau_x[kk+4]-ZZ)*(ZZ-tau_x[kk+1])*(tau_x[kk+3]-ZZ)*(tau_x[kk+2]<=ZZ)*(ZZ<tau_x[kk+3])+
+    #       (1/6/dx^3)*(tau_x[kk+4]-ZZ)*(tau_x[kk+4]-ZZ)*(ZZ-tau_x[kk+2])*(tau_x[kk+2]<=ZZ)*(ZZ<tau_x[kk+3])+
+    #       (1/6/dx^3)*(tau_x[kk+4]-ZZ)*(tau_x[kk+4]-ZZ)*(tau_x[kk+4]-ZZ)*(tau_x[kk+3]<=ZZ)*(ZZ<tau_x[kk+4])
+    #   }
+    #
+    #Axel/check-spline-algorithm, in Matlab this for loop does not run. Change to 4:-1:3 in Matlab?
+    # % i = kx + 2
+    XX[kx+2,] =  -(1/6/dx^3)*(tau_x[kx+2]-ZZ)*(tau_x[kx+2]-ZZ)*(tau_x[kx+2]-ZZ)*(tau_x[kx+2]<=ZZ)*(ZZ<tau_x[kx+3])-
+        (1/6/dx^3)*(tau_x[kx+2]-ZZ)*(tau_x[kx+2]-ZZ)*(ZZ-tau_x[kx+4])*(tau_x[kx+3]<=ZZ)*(ZZ<tau_x[kx+4])-
+        (1/6/dx^3)*(tau_x[kx+2]-ZZ)*(ZZ-tau_x[kx+5])*(tau_x[kx+3]-ZZ)*(tau_x[kx+3]<=ZZ)*(ZZ<tau_x[kx+4])-
+        (1/6/dx^3)*(tau_x[kx+2]-ZZ)*(ZZ-tau_x[kx+5])*(ZZ-tau_x[kx+5])*(tau_x[kx+4]<=ZZ)*(ZZ<tau_x[kx+5])-
+        (1/4/dx^3)*(ZZ-tau_x[kx+6])*(tau_x[kx+3]-ZZ)*(tau_x[kx+3]-ZZ)*(tau_x[kx+3]<=ZZ)*(ZZ<tau_x[kx+4])-
+        (1/4/dx^3)*(ZZ-tau_x[kx+6])*(tau_x[kx+3]-ZZ)*(ZZ-tau_x[kx+5])*(tau_x[kx+4]<=ZZ)*(ZZ<tau_x[kx+5])-
+        (1/2/dx^3)*(ZZ-tau_x[kx+6])*(ZZ-tau_x[kx+6])*(tau_x[kx+4]-ZZ)*(tau_x[kx+4]<=ZZ)*(ZZ<tau_x[kx+5])
+
+    # % i = kx + 3
+    XX[kx+3,] = - (1/4/dx^3)*(tau_x[kx+3]-ZZ)*(tau_x[kx+3]-ZZ)*(tau_x[kx+3]-ZZ)*(tau_x[kx+3]<=ZZ)*(ZZ<tau_x[kx+4])-
+        (1/4/dx^3)*(tau_x[kx+3]-ZZ)*(tau_x[kx+3]-ZZ)*(ZZ-tau_x[kx+5])*(tau_x[kx+4]<=ZZ)*(ZZ<tau_x[kx+5])-
+        (1/2/dx^3)*(tau_x[kx+3]-ZZ)*(ZZ-tau_x[kx+6])*(tau_x[kx+4]-ZZ)*(tau_x[kx+4]<=ZZ)*(ZZ<tau_x[kx+5])-
+        (1/dx^3)*(ZZ-tau_x[kx+7])*(tau_x[kx+4]-ZZ)*(tau_x[kx+4]-ZZ)*(tau_x[kx+4]<=ZZ)*(ZZ<tau_x[kx+5])
+
+    # % i = kx + 4
+    XX[kx+4,] = -(1/dx^3)*(tau_x[kx+4]-ZZ)*(tau_x[kx+4]-ZZ)*(tau_x[kx+4]-ZZ)*(tau_x[kx+4]<=ZZ)*(ZZ<=tau_x[kx+5])
+
+    XX = t(XX)
 
   return(XX)
 }
