4bfce565164e1cfd2971f98ada7c9dc97fdfe680
Solvi Rognvaldsson <solviro@gmail.com>
2020-02-11 21:50:42 +0000

    added comments to explain the steps

--- R/S3_test.R
+++ R/S3_test.R
@@ -1,6 +1,10 @@
 #S3 object test
+#collect a list of results from gbplm
 results_obj=list('theta'=1:9,'fit'=1:100,'lower'=2:101,'upper'=0:99,beta=rnorm(100))
+#set class of the list to be gbplm
 attr(results_obj, "class") <- "gbplm"
+
+#implement generic functions for the class
 print.gbplm <- function(x,...){
     return('þetta er svolítið sniðugt')
 }
