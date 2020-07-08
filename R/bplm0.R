b9662b2d3f0a68de929ac227f20aad2bd787de7c
axelornj <axelorn94@gmail.com>
2020-07-08 22:19:45 +0000

    Fixed w_unoberserved bplm0

--- R/bplm0.R
+++ R/bplm0.R
@@ -21,9 +21,9 @@ bplm0 <- function(formula,data,c_param=NULL,w_limits=NULL,country="Iceland",forc
     Q <- model_dat[,1,drop=T]
     w <- model_dat[,2,drop=T]
     MCMC_output_list <- bplm0.inference(y=log(Q),w=w,c_param,w_limits,country,forcepoint,...)
-    rating_curve <- data.frame(W=MCMC_output_list$W,as.data.frame(t(apply(MCMC_output_list$ypo,1,quantile, probs = c(0.025,0.5, 0.975),na.rm=T))),row.names=NULL)
-    names(rating_curve) <- c('W','lower','median','upper')
-    rating_curve <- rating_curve[order(rating_curve$W),]
+    rating_curve <- data.frame(w=MCMC_output_list$w_full,as.data.frame(t(apply(MCMC_output_list$ypo,1,quantile, probs = c(0.025,0.5, 0.975),na.rm=T))),row.names=NULL)
+    names(rating_curve) <- c('w','lower','median','upper')
+    rating_curve <- rating_curve[order(rating_curve$w),]
     param_summary <- as.data.frame(t(apply(rbind(MCMC_output_list$a,MCMC_output_list$b,MCMC_output_list$theta),1,quantile, probs = c(0.025,0.5, 0.975),na.rm=T)))
     names(param_summary) <- c('lower','median','upper')
     param_names <- 'var_epsilon'
@@ -39,7 +39,7 @@ bplm0 <- function(formula,data,c_param=NULL,w_limits=NULL,country="Iceland",forc
     result_obj=list()
     result_obj$formula <- formula
     result_obj$data <- model_dat
-    result_obj$W_full <- W
+    result_obj$w_full <- MCMC_output_list$w_full
     result_obj$post_a = MCMC_output_list$a
     result_obj$post_b = MCMC_output_list$b
     if(!is.null(RC$c)){
@@ -47,7 +47,6 @@ bplm0 <- function(formula,data,c_param=NULL,w_limits=NULL,country="Iceland",forc
         result_obj$var_epsilon <- MCMC_output_list$theta[2,]
     }else{
         result_obj$post_c <- NULL
-        print(MCMC_output_list$theta)
         result_obj$var_epsilon <- MCMC_output_list$theta[1,]
     }
     result_obj$DIC <- DIC
@@ -151,40 +150,14 @@ bplm0.inference <- function(y,w,c_param=NULL,w_limits=NULL,country="Iceland",for
       return(output_mat)
     }
     stopCluster(cl)
-    MCMC_output_list <- list('W'=c(RC$w,RC$w_u),
-                             'theta'=MCMC_output_mat[1:theta_length,drop=F],
+    MCMC_output_list <- list('w_full'=c(RC$w,RC$w_u),
+                             'theta'=MCMC_output_mat[1:theta_length,,drop=F],
                              'a'=exp(MCMC_output_mat[theta_length+1,]),
                              'b'=MCMC_output_mat[theta_length+2,],
                              'ypo'=exp(MCMC_output_mat[(theta_length+2+1):(nrow(MCMC_output_mat)-1),]),
                              'DIC'=MCMC_output_mat[nrow(MCMC_output_mat),])
     
     return(MCMC_output_list)
-}
-
-print.bplm0 <- function(x,...){
-    cat("\nCall:\n",
-        paste(deparse(x$formula), sep = "\n", collapse = "\n"), "\n\n", sep = "")
-}
-
-
-summary.bplm0 <- function(x,...){
-    names(x$param_summary)=paste0(names(x$param_summary),c('(2.5%)','(50%)','(97.5%)'))
-    cat("\nFormula: \n",
-        paste(deparse(x$formula), sep = "\n", collapse = "\n"))
-    cat("\nLatent parameters:\n")
-    print(x$param_summary[1:2,],row.names = T,digits=3,right=F)
-    cat("\nHyperparameters:\n")
-    print(x$param_summary[3:nrow(param_summary),],row.names = T,digits=3,right=F)
-    cat("\nDIC:",x$DIC[2])
-}
-
-plot.bplm0 <- function(x,type='rating_curve',xlab='Q',ylab='W',...){
-    if(type=='rating_curve'){
-        plot(x$rating_curve$median,sort(x$W_full),type='l')
-        points(x$data[,1,drop=T],x$data[,2,drop=T],...)
-    }else if(type=='beta'){
-        plot(sort(x$beta$W),type='l',...)
-    }
 }
 
 #create a predict method for interpolation of posterior predictive
@@ -197,33 +170,19 @@ plot.bplm0 <- function(x,type='rating_curve',xlab='Q',ylab='W',...){
 #'@return Returns a list containing predictive values of the parameters drawn out of the evaluated density.
 #'@references Birgir Hrafnkelsson, Helgi Sigurdarson and Sigurdur M. Gardarson (2015) \emph{Bayesian Generalized Rating Curves}
 bplm0.density_evaluation_known_c <- function(theta,RC){
-  #print(length(th))
     log_sig_eps2 <- theta[1]
-  
-    #Sig_eps = exp(log_sig_eps2)*diag(RC$epsilon)
-    
-    
+
     l=c(log(RC$w-RC$c))
 
     X=cbind(rep(1,length(l)),l)
 
-    
-    
-    L=t(chol(RC$Sig_xinv + (t(X) %*% X)/exp(log_sig_eps2))) # fix
-    #L = t(chol(X%*%RC$Sig_x%*%t(X)+Sig_eps))
-    
+    L=t(chol(RC$Sig_xinv + (t(X) %*% X)/exp(log_sig_eps2))) 
+
     q=solve(L,(RC$Sinvmu+t(X)%*%RC$y/exp(log_sig_eps2)))
-    #w = solve(L,RC$y-X%*%RC$mu_x))
-    
+
     p=0.5*sum(q^2)+log(L[1,1])+log(L[2,2])-
-     0.5*sum(RC$y^2)/exp(log_sig_eps2)-RC$n*log_sig_eps2/2 - log_sig_eps2 # fix -exp(th[1])*RC$mu_c
-    #p = - log(L[1,1])-log(L[2,2])-0.5*t(w)%*%w
-    #    - RC$n*log_sig_eps2/2 - log_sig_eps2
-     
-    #W = solve(L,X%*%RC$Sig_x)
-    
-    #x = RC$mu_x + t(W)%*%w + rnorm(2,sd=sqrt(c(RC$Sig_x-t(W)%*%W)))
-      
+     0.5*sum(RC$y^2)/exp(log_sig_eps2)-RC$n*log_sig_eps2/2 - log_sig_eps2
+
     x=solve(t(L),(q+as.matrix(rnorm(2))))
     
     yp=X %*% x
@@ -243,25 +202,13 @@ bplm0.density_evaluation_known_c <- function(theta,RC){
 #'@return Returns a list containing predictive values of the parameters drawn out of the evaluated density.
 #'@references Birgir Hrafnkelsson, Helgi Sigurdarson and Sigurdur M. Gardarson (2015) \emph{Bayesian Generalized Rating Curves}
 bplm0.density_evaluation_unknown_c <- function(theta,RC){
-    
-    #f=lambda[1:5]-lambda[6]
-   # zeta - log(hmin - c) 
     zeta <- theta[1]
     log_sig_eps2 <- theta[2]
-  
-    #Sig_eps = exp(log_sig_eps2)*diag(RC$epsilon)
-    
-    l=c(log(RC$w+exp(zeta)))
-    
-    
-    
+    l=c(log(RC$w-RC$w_min+exp(zeta)))
     
     X=cbind(rep(1,length(l)),l)
-    
-    L=t(chol(RC$Sig_xinv + (t(X) %*% X)/exp(log_sig_eps2))) # fix
-   
-    #L = t(chol(X%*%RC$Sig_x%*%t(X)+Sig_eps))
-    
+    L=t(chol(RC$Sig_xinv + (t(X) %*% X)/exp(log_sig_eps2)))
+
     q=solve(L,(RC$Sinvmu+t(X)%*%RC$y/exp(log_sig_eps2)))
     
     p=0.5*sum(q^2)+log(L[1,1])+log(L[2,2])-
@@ -295,7 +242,8 @@ bplm0.predict_u_known_c <- function(param,RC){
   #collecting parameters from the MCMC sample
   theta=param[1] 
   log_sig_eps2 = theta[1]
-  x=param[2:length(param)] # a and b
+  #a and b
+  x=param[2:length(param)]
   
   m = length(RC$w_u)
   #building blocks of the explanatory matrix X calculated
@@ -323,10 +271,12 @@ bplm0.predict_u_unknown_c <- function(param,RC){
     theta=param[1:2] 
     zeta=theta[1]
     log_sig_eps2 = theta[2]
-    x=param[3:length(param)] # a and b
+    
+    #a and b
+    x=param[3:length(param)] 
     
     m=length(RC$w_u)
-    above_c <- -(exp(zeta)-RC$w_min) < RC$w_u
+    above_c <- RC$w_min-exp(zeta) < RC$w_u
     m_above_c <- sum(above_c)
     #building blocks of the explanatory matrix X calculated
     l=log(RC$w_u[above_c]-RC$w_min+exp(zeta))
