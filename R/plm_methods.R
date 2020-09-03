93117c8c16a60beb6c357054e1292703909e4d8d
Sölvi Rögnvaldsson <sor16@hi.is>
2020-09-03 21:16:12 +0000

    changes due to output changes

--- R/plm_methods.R
+++ R/plm_methods.R
@@ -13,7 +13,7 @@ summary.bplm0 <- function(x,...){
     print(x$param_summary[1:2,],row.names = T,digits=3,right=F)
     cat("\nHyperparameters:\n")
     print(x$param_summary[3:nrow(x$param_summary),],row.names = T,digits=3,right=F)
-    cat("\nDIC:",x$DIC[2])
+    cat("\nDIC:",x$DIC_summary[,2])
 }
 
 plot.bplm0 <- function(x,type='rating_curve',xlab='Q',ylab='W',...){
@@ -40,7 +40,7 @@ summary.bplm <- function(x,...){
     print(x$param_summary[1:2,],row.names = T,digits=3,right=F)
     cat("\nHyperparameters:\n")
     print(x$param_summary[3:nrow(x$param_summary),],row.names = T,digits=3,right=F)
-    cat("\nDIC:",x$DIC[2])
+    cat("\nDIC:",x$DIC_summary[,2])
 }
 
 plot.bplm <- function(x,type='rating_curve',xlab='Q',ylab='W',...){
@@ -51,6 +51,28 @@ plot.bplm <- function(x,type='rating_curve',xlab='Q',ylab='W',...){
         plot(sort(x$beta$W),type='l',...)
     }
 }
+
+
+summary.bgplm0 <- function(x,...){
+    names(x$param_summary)=paste0(names(x$param_summary),c('-2.5%','-50%','-97.5%'))
+    cat("\nFormula: \n",
+        paste(deparse(x$formula), sep = "\n", collapse = "\n"))
+    cat("\nLatent parameters:\n")
+    print(x$param_summary[1:2,],row.names = T,digits=3,right=F)
+    cat("\nHyperparameters:\n")
+    print(x$param_summary[3:nrow(x$param_summary),],row.names = T,digits=3,right=F)
+    cat("\nDIC:",x$DIC_summary[,2])
+}
+
+plot.bgplm0 <- function(x,type='rating_curve',xlab='Q',ylab='W',...){
+    if(type=='rating_curve'){
+        plot(x$rating_curve$median,x$rating_curve$W,type='l')
+        points(x$data[,1,drop=T],x$data[,2,drop=T],...)
+    }else if(type=='beta'){
+        plot(sort(x$beta$W),type='l',...)
+    }
+}
+
 
 
 print.bgplm <- function(x,...){
@@ -67,7 +89,7 @@ summary.bgplm <- function(x,...){
     print(x$param_summary[1:2,],row.names = T,digits=3,right=F)
     cat("\nHyperparameters:\n")
     print(x$param_summary[3:nrow(x$param_summary),],row.names = T,digits=3,right=F)
-    cat("\nDIC:",x$DIC[2])
+    cat("\nDIC:",x$DIC_summary[,2])
 }
 
 plot.bgplm <- function(x,type='rating_curve',xlab='Q',ylab='W',...){
