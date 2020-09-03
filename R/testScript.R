0d294d3694fa2e16f98ca743ff1c09e76199ce76
Sölvi Rögnvaldsson <sor16@hi.is>
2020-09-03 21:16:22 +0000

    minor changes

--- R/testScript.R
+++ R/testScript.R
@@ -1,15 +1,15 @@
 library(readxl)
 library(dplyr)
 library(ggplot2)
-source('R/Adist.R')
 source('R/B_splines.R')
 source('R/W_unobserved.R')
-source('R/priors.R')
+source('R/help_functions.R')
+source('R/plm_methods.R')
 source('R/bplm0.R')
 source('R/bplm.R')
 source('R/bgplm0.R')
 source('R/bgplm.R')
-source('R/plm_methods.R')
+
 
 #read data
 rc_dat <- read_excel('data/exceldata_RC.xlsx') %>% mutate(W=0.01*W)
@@ -22,7 +22,7 @@ summary(bplm0.fit)
 
 
 ggplot(data=bplm0.fit$rating_curve) +
-  geom_point(data=rc_dat,aes(Q,w)) +
+  geom_point(data=rc_dat,aes(Q,W)) +
   geom_line(aes(median,w)) +
   geom_line(aes(lower,w),linetype='dashed') +
   geom_line(aes(upper,w),linetype='dashed')
@@ -33,7 +33,7 @@ bplm0.fit_known_c <- bplm0(rc_formula,rc_dat,c_param=0.75)
 summary(bplm0.fit_known_c)
 
 ggplot(data=bplm0.fit_known_c$rating_curve) +
-  geom_point(data=rc_dat,aes(Q,w)) +
+  geom_point(data=rc_dat,aes(Q,W)) +
   geom_line(aes(median,w)) +
   geom_line(aes(lower,w),linetype='dashed') +
   geom_line(aes(upper,w),linetype='dashed')
@@ -71,18 +71,28 @@ ggplot(data=bgplm0.fit$rating_curve) +
     geom_line(aes(lower,w),linetype='dashed') +
     geom_line(aes(upper,w),linetype='dashed')
 
-bgplm.fit_known_c <- bgplm0(rc_formula,rc_dat,c_param = 0.75)
+bgplm0.fit_known_c <- bgplm0(rc_formula,rc_dat,c_param = 0.75)
 
 summary(bgplm0.fit_known_c)
 
 ggplot(data=bgplm0.fit_known_c$rating_curve) +
-    geom_point(data=rc_dat,aes(Q,W)) +
-    geom_line(aes(median,W)) +
-    geom_line(aes(lower,W),linetype='dashed') +
-    geom_line(aes(upper,W),linetype='dashed')
+    geom_point(data=rc_dat,aes(Q,w)) +
+    geom_line(aes(median,w)) +
+    geom_line(aes(lower,w),linetype='dashed') +
+    geom_line(aes(upper,w),linetype='dashed')
 
 
 ####bgplm#####
+bgplm.fit<- bgplm(rc_formula,rc_dat)
+
+summary(bgplm.fit)
+
+ggplot(data=bgplm.fit$rating_curve) +
+  geom_point(data=rc_dat,aes(Q,W)) +
+  geom_line(aes(median,w)) +
+  geom_line(aes(lower,w),linetype='dashed') +
+  geom_line(aes(upper,w),linetype='dashed')
+
 bgplm.fit_known_c <- bgplm(rc_formula,rc_dat,c_param = 0.75)
 
 summary(bgplm.fit_known_c)
@@ -93,15 +103,7 @@ ggplot(data=bgplm.fit_known_c$rating_curve) +
     geom_line(aes(lower,w),linetype='dashed') +
     geom_line(aes(upper,w),linetype='dashed')
 
-ggplot(data=filter(bgplm.fit_known_c$beta,W>=min(rc_dat$W),W<=max(rc_dat$W))) +
-    geom_line(aes(W,median)) +
-    geom_line(aes(W,lower),linetype='dashed') +
-    geom_line(aes(W,upper),linetype='dashed')
 
-ggplot(data=filter(bgplm.fit_known_c$sigma_eps,W>=min(rc_dat$W),W<=max(rc_dat$W))) +
-    geom_line(aes(W,median)) +
-    geom_line(aes(W,lower),linetype='dashed') +
-    geom_line(aes(W,upper),linetype='dashed')
 
 
 
