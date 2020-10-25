a6a430dfb5a571186ad77747717e4bf8e99a22ad
Sölvi Rögnvaldsson <sor16@hi.is>
2020-10-25 18:16:07 +0000

    package related things added regarding to documentation

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Data from x river in Iceland
+#'
+#' @source Icelandic Meteorological Office. \url{https://en.vedur.is/}
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{W}{Measurements of river stage}
+#'  \item{Q}{Measurements of river discharge}
+#' }
+#' @examples
+#' \dontrun{
+#'  V316_river
+#' }
+"V316_river"
