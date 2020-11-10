f02eb4b54bb59f63e2cbea13b7cd4430fa7cbb98
Sölvi Rögnvaldsson <sor16@hi.is>
2020-11-10 20:14:49 +0000

    add support for c param when c is known

--- R/spread_draws.R
+++ R/spread_draws.R
@@ -25,7 +25,12 @@ spread_draws <- function(mod,param,transformed=F){
         MCMC_output <- mod[[list_name]]
         if(is.null(dim(MCMC_output))){
             if(transformed){
-                MCMC_output <- get_transformed_param(MCMC_output,param,mod)
+                if(param=='c'){
+                    w_min <- min(mod$data[[all.vars(mod$formula)[2]]])
+                    MCMC_output <- get_transformed_param(MCMC_output,param,mod,w_min=w_min)
+                }else{
+                    MCMC_output <- get_transformed_param(MCMC_output,param,mod)
+                }
             }else{
                 names(MCMC_output) <- rep(param,length(MCMC_output))
             }
@@ -33,23 +38,29 @@ spread_draws <- function(mod,param,transformed=F){
                                   iter=rep(1:(length(MCMC_output)/num_chains)))
             out_dat[,unique(names(MCMC_output))] <- MCMC_output
         }else{
+            c_hat <- if(!is.null(mod$run_info_c_param)) median(mod$c_posterior) else mod$run_info$c_param
             out_dat_list <- lapply(1:nrow(MCMC_output),
                                    function(i){
                                         out_dat <- data.frame(chain=rep(1:num_chains,rep(ncol(MCMC_output)/num_chains,num_chains)),
-                                                              iter=rep(1:(ncol(MCMC_output)/num_chains),num_chains),W=mod$rating_curve$w[i])
+                                                              iter=rep(1:(ncol(MCMC_output)/num_chains),num_chains),w=ifelse(transformed,log(mod$rating_curve$w[i]-c_hat),mod$rating_curve$w[i]))
                                         out_dat[,param] <- MCMC_output[i,,drop=T]
                                         return(out_dat)
                                    })
             out_dat <- do.call('rbind',out_dat_list)
         }
     }else if(param %in% c('latent_parameters','hyperparameters')){
-        params <- get_param_names(class(mod),!is.null(mod$c_posterior))
+        params <- get_param_names(class(mod),c_param=mod$run_info$c_param)
         param_idx <- if(param=='latent_parameters') 1:2 else 3:length(params)
         out_dat_list <- lapply(params[param_idx],function(param_name){
             list_name <- paste0(param_name,'_posterior')
             MCMC_output <- mod[[list_name]]
             if(transformed){
-                MCMC_output <- get_transformed_param(MCMC_output,param_name,mod)
+                if(param_name=='c'){
+                    w_min <- min(mod$data[[all.vars(mod$formula)[2]]])
+                    MCMC_output <- get_transformed_param(MCMC_output,param_name,mod,w_min=w_min)
+                }else{
+                    MCMC_output <- get_transformed_param(MCMC_output,param_name,mod)
+                }
             }else{
                 names(MCMC_output) <- rep(param_name,length(MCMC_output))
             }
