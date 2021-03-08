969c7379f5ca236ff132ad7b56b6ab17fab9ac8e
Sölvi Rögnvaldsson <sor16@hi.is>
2021-03-08 19:35:47 +0000

    test file for extract draw completed

---
+++ tests/testthat/test-extract_draws.R
@@ -0,0 +1,86 @@
+context('Advanced data wrangling of bdrc model objects')
+set.seed(1)
+bplm0.fit <- bplm0(Q~W,V316_river)
+bplm.fit <- bplm(Q~W,V316_river)
+bgplm0.fit <- bgplm0(Q~W,V316_river)
+bgplm.fit <- bgplm(Q~W,V316_river)
+
+bplm0.fit_known_c <- bplm0(Q~W,V316_river,c_param=0.8)
+
+check_spread_draws_MCMC_mat_concordance <- function(mod,dat){
+    components <- unique(names(dat)[!(names(dat) %in% c('chain','iter','h'))])
+    for(x in components){
+        MCMC_output <- mod[[paste0(x,'_posterior')]]
+        if(is.null(dim(MCMC_output))){
+            if('h' %in% names(dat)){
+                expect_equal(dat[[x]][dat$h==dat$h[1]],MCMC_output)
+            }else{
+                expect_equal(dat[[x]],MCMC_output)
+            }
+        }else{
+            expect_equal(dat[[x]],c(t(MCMC_output)))
+        }
+    }
+}
+
+check_gather_draws_MCMC_mat_concordance <- function(mod,dat){
+    components <- unique(dat$name)
+    for(x in components){
+        MCMC_output <- mod[[paste0(x,'_posterior')]]
+        curr_dat <- dat[dat$name==x,]
+        if(is.null(dim(MCMC_output))){
+            if('h' %in% names(curr_dat)){
+                expect_equal(curr_dat$value[curr_dat$h==curr_dat$h[1]],MCMC_output)
+            }else{
+                expect_equal(curr_dat$value,MCMC_output)
+            }
+        }else{
+            expect_equal(curr_dat$value,c(t(MCMC_output)))
+        }
+    }
+}
+
+test_that("check different inputs to spread draws and gather draws", {
+    expect_equal(spread_draws(bplm0.fit,'c','sigma_eps'),spread_draws(bplm0.fit,c('c','sigma_eps')))
+    expect_equal(spread_draws(bplm.fit,'c',paste0('eta_',1:3)),spread_draws(bplm.fit,c('c','c',paste0('eta_',1:3))))
+    expect_equal(spread_draws(bgplm0.fit,'c','c'),spread_draws(bgplm0.fit,'c'))
+    expect_equal(spread_draws(bgplm.fit,c('c','sigma_beta'),c('eta_1','sigma_eta')),spread_draws(bgplm.fit,'c','sigma_beta','eta_1','sigma_eta'))
+    expect_error(spread_draws(bgplm.fit_known_c,'c'))
+
+    expect_equal(gather_draws(bplm0.fit,'c','sigma_eps'),gather_draws(bplm0.fit,c('c','sigma_eps')))
+    expect_equal(gather_draws(bplm.fit,'c',paste0('eta_',1:3)),gather_draws(bplm.fit,c('c','c',paste0('eta_',1:3))))
+    expect_equal(gather_draws(bgplm0.fit,'c','c'),gather_draws(bgplm0.fit,'c'))
+    expect_equal(gather_draws(bgplm.fit,c('c','sigma_beta'),c('eta_1','sigma_eta')),gather_draws(bgplm.fit,'c','sigma_beta','eta_1','sigma_eta'))
+    expect_error(gather_draws(bgplm.fit_known_c,'c'))
+})
+
+test_that("spread draws and gather draws are concordant with MCMC matrices", {
+    #spread_draws
+    check_spread_draws_MCMC_mat_concordance(bplm0.fit,spread_draws(bplm0.fit,'c','sigma_eps'))
+    check_spread_draws_MCMC_mat_concordance(bplm0.fit,spread_draws(bplm0.fit,'rating_curve','rating_curve_mean','c'))
+
+    check_spread_draws_MCMC_mat_concordance(bplm.fit,spread_draws(bplm.fit,'c',paste0('eta_',1:6)))
+    check_spread_draws_MCMC_mat_concordance(bplm.fit,spread_draws(bplm.fit,'rating_curve','rating_curve_mean','sigma_eps','sigma_eta'))
+
+    check_spread_draws_MCMC_mat_concordance(bgplm0.fit,spread_draws(bgplm0.fit,'c','phi_beta','sigma_beta'))
+    check_spread_draws_MCMC_mat_concordance(bgplm0.fit,spread_draws(bgplm0.fit,'rating_curve','rating_curve_mean','beta','f','sigma_eps'))
+
+    check_spread_draws_MCMC_mat_concordance(bgplm.fit,spread_draws(bgplm.fit,'c',paste0('eta_',1:6)))
+    check_spread_draws_MCMC_mat_concordance(bgplm.fit,spread_draws(bgplm.fit,'rating_curve','rating_curve_mean','sigma_eps','beta','f','sigma_eta'))
+    #gather_draws
+    check_gather_draws_MCMC_mat_concordance(bplm0.fit,spread_draws(bplm0.fit,'c','sigma_eps'))
+    check_gather_draws_MCMC_mat_concordance(bplm0.fit,spread_draws(bplm0.fit,'rating_curve','rating_curve_mean','c'))
+
+    check_gather_draws_MCMC_mat_concordance(bplm.fit,spread_draws(bplm.fit,'c',paste0('eta_',1:6)))
+    check_gather_draws_MCMC_mat_concordance(bplm.fit,spread_draws(bplm.fit,'rating_curve','rating_curve_mean','sigma_eps','sigma_eta'))
+
+    check_gather_draws_MCMC_mat_concordance(bgplm0.fit,spread_draws(bgplm0.fit,'c','phi_beta','sigma_beta'))
+    check_gather_draws_MCMC_mat_concordance(bgplm0.fit,spread_draws(bgplm0.fit,'rating_curve','rating_curve_mean','beta','f','sigma_eps'))
+
+    check_gather_draws_MCMC_mat_concordance(bgplm.fit,spread_draws(bgplm.fit,'c',paste0('eta_',1:6)))
+    check_gather_draws_MCMC_mat_concordance(bgplm.fit,spread_draws(bgplm.fit,'rating_curve','rating_curve_mean','sigma_eps','beta','f','sigma_eta'))
+})
+
+
+
+
