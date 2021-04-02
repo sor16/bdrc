8fc17bdf13561102f2ffb7fd12e9d1f578c1cab0
Sölvi Rögnvaldsson <sor16@hi.is>
2021-04-02 09:25:02 +0000

    tournament tests added

--- tests/testthat/test-tournament.R
+++ tests/testthat/test-tournament.R
@@ -1,5 +1,8 @@
 context('Tournament')
-# t_obj <- tournament(Q~W,V316_river)
 # test_that("the tournament object is in tact", {
+#     expect_is(t_obj,"tournament")
+# })
+
+# test_that("tournament can handle different inputs", {
 #     expect_is(t_obj,"tournament")
 # })
