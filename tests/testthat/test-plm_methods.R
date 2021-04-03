8d5bc8e12f57e7563acd1ecdbc9a9ed906980d4d
Sölvi Rögnvaldsson <sor16@hi.is>
2021-04-03 09:02:02 +0000

    skip vdiffr tests on ci and cran

--- tests/testthat/test-plm_methods.R
+++ tests/testthat/test-plm_methods.R
@@ -1,6 +1,8 @@
 context('plm general functions')
 
 test_that("trace plots have known output", {
+    skip_on_cran()
+    skip_on_ci()
     expect_doppelganger('bplm0_trace_plot_hyperparameters',autoplot(bplm0.fit,type='trace','hyperparameters'))
     expect_doppelganger('bplm0_trace_plot_hyperparameters_transformed',autoplot(bplm0.fit,type='trace','hyperparameters',transformed=T))
     expect_doppelganger('bplm0_trace_plot_single_parameter',autoplot(bplm0.fit,type='trace','sigma_eps'))
@@ -18,6 +20,8 @@ test_that("trace plots have known output", {
     expect_doppelganger('bgplm_trace_plot_single_parameter',autoplot(bgplm.fit,type='trace','sigma_eta'))
 })
 test_that("histogram have known output", {
+    skip_on_cran()
+    skip_on_ci()
     expect_doppelganger('bplm0_histogram_hyperparameters',autoplot(bplm0.fit,type='histogram','hyperparameters'))
     expect_doppelganger('bplm0_histogram_hyperparameters_transformed',autoplot(bplm0.fit,type='histogram','hyperparameters',transformed=T))
     expect_doppelganger('bplm0_histogram_single_parameter',autoplot(bplm0.fit,type='histogram','sigma_eps'))
@@ -36,6 +40,8 @@ test_that("histogram have known output", {
 })
 
 test_that("rating_curve has known output", {
+    skip_on_cran()
+    skip_on_ci()
     expect_doppelganger('bplm0_rating_curve',autoplot(bplm0.fit,type='rating_curve'))
     expect_doppelganger('bplm0_rating_curve_transformed',autoplot(bplm0.fit,type='rating_curve',transformed=T))
 
@@ -50,6 +56,8 @@ test_that("rating_curve has known output", {
 })
 
 test_that("rating_curve_mean has known output", {
+    skip_on_cran()
+    skip_on_ci()
     expect_doppelganger('bplm0_rating_curve_mean',autoplot(bplm0.fit,type='rating_curve_mean'))
     expect_doppelganger('bplm0_rating_curve_mean_transformed',autoplot(bplm0.fit,type='rating_curve_mean',transformed=T))
 
@@ -65,6 +73,8 @@ test_that("rating_curve_mean has known output", {
 
 
 test_that("sigma_eps has known output", {
+    skip_on_cran()
+    skip_on_ci()
     expect_doppelganger('bplm0_sigma_eps',autoplot(bplm0.fit,type='sigma_eps'))
     expect_doppelganger('bplm_sigma_eps',autoplot(bplm.fit,type='sigma_eps'))
     expect_doppelganger('bgplm0_sigma_eps',autoplot(bgplm0.fit,type='sigma_eps'))
@@ -72,6 +82,8 @@ test_that("sigma_eps has known output", {
 })
 
 test_that("beta has known output", {
+    skip_on_cran()
+    skip_on_ci()
     expect_error(autoplot(bplm0.fit,type='beta'))
     expect_error(autoplot(bplm.fit,type='beta'))
     expect_doppelganger('bgplm0_beta',autoplot(bgplm0.fit,type='beta'))
@@ -79,6 +91,8 @@ test_that("beta has known output", {
 })
 
 test_that("f has known output", {
+    skip_on_cran()
+    skip_on_ci()
     expect_doppelganger('bplm0_f',autoplot(bplm0.fit,type='f'))
     expect_doppelganger('bplm_f',autoplot(bplm.fit,type='f'))
     expect_doppelganger('bgplm0_f',autoplot(bgplm0.fit,type='f'))
@@ -86,6 +100,8 @@ test_that("f has known output", {
 })
 
 test_that("residuals has known output", {
+    skip_on_cran()
+    skip_on_ci()
     expect_doppelganger('bplm0_residuals',autoplot(bplm0.fit,type='residuals'))
     expect_doppelganger('bplm_residuals',autoplot(bplm.fit,type='residuals'))
     expect_doppelganger('bgplm0_residuals',autoplot(bgplm0.fit,type='residuals'))
