4d5ed7d944018cae9608b421e5c89c91eeee0e80
Sölvi Rögnvaldsson <sor16@hi.is>
2021-04-10 10:35:27 +0000

    naming change p2

--- tests/testthat/test-tournament.R
+++ tests/testthat/test-tournament.R
@@ -1,16 +1,23 @@
 context('Tournament')
 
 test_that("tournament can handle different inputs", {
-    expect_error(tournament(bplm0.fit,bplm0.fit,bgplm0.fit,bgplm.fit))
-    expect_error(tournament(bplm0.fit,bplm.fit,bgplm0.fit))
+    expect_error(tournament(plm0.fit,plm0.fit,gplm0.fit,gplm.fit))
+    expect_error(tournament(plm0.fit,plm.fit,gplm0.fit))
 })
 
 test_that("the tournament object is in tact", {
     expect_is(t_obj,"tournament")
-    expect_is(t_obj$contestants$bplm0,"bplm0")
-    expect_is(t_obj$contestants$bplm,"bplm")
-    expect_is(t_obj$contestants$bgplm0,"bgplm0")
-    expect_is(t_obj$contestants$bgplm,"bgplm")
+    expect_is(t_obj$contestants$plm0,"plm0")
+    expect_is(t_obj$contestants$plm,"plm")
+    expect_is(t_obj$contestants$gplm0,"gplm0")
+    expect_is(t_obj$contestants$gplm,"gplm")
+    expect_true(all(sapply(1:nrow(t_obj$summary),function(i) t_obj$contestants[[t_obj$summary$model[i]]]$DIC==t_obj$summary$DIC[i])))
 })
 
+test_that("Tournament object remains the same", {
+    skip_on_cran()
+    skip_on_ci()
+    skip_on_covr()
+    expect_equal_to_reference(t_obj,file='../cached_results/tournament.rds',update=T)
+})
 
