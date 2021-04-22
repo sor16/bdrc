9b03aba67663c3d6e9a183bfe6cf3e91ca7c5f82
Sölvi Rögnvaldsson <sor16@hi.is>
2021-04-22 19:51:53 +0000

    naming change in pacakge file

--- R/bdrc.R
+++ R/bdrc.R
@@ -2,13 +2,13 @@
 #'
 #' The package implements the following Bayesian hierarchical discharge rating curve models for paired measurements of stage and discharge in rivers described in Hrafnkelsson et al.:
 #'
-#' \code{\link{bplm0}} - Power-law model with constant variance
+#' \code{\link{plm0}} - Power-law model with constant variance
 #'
-#' \code{\link{bplm}} - Power-law model with variance that may vary with stage
+#' \code{\link{plm}} - Power-law model with variance that may vary with stage
 #'
-#' \code{\link{bgplm0}} - Generalized power-law model with constant variance
+#' \code{\link{gplm0}} - Generalized power-law model with constant variance
 #'
-#' \code{\link{bgplm}} - Generalized power-law model with variance that may vary with stage
+#' \code{\link{gplm}} - Generalized power-law model with variance that may vary with stage
 #'
 #' @docType package
 #' @name bdrc
