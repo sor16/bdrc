353309e60e7c52569c2aa8564793617b8064a08e
Sölvi Rögnvaldsson <sor16@hi.is>
2021-05-01 15:16:23 +0000

    changed arguments in tournament and documentation respectively. Also added num effective parameters in summary

--- R/tournament.R
+++ R/tournament.R
@@ -13,10 +13,12 @@ evaluate_game <- function(m){
     BF <- B_vec[1]/B_vec[2]
     PR_m1 <- 1/(1+(1/BF))
     DIC_vec <- sapply(m,function(x) x$DIC)
+    num_eff_param <- sapply(m,function(x) x$num_effective_param)
     winner <- ifelse(PR_m1>=0.75,1,2)
     data.frame(model=sapply(m,class),
                B=B_vec,
                DIC=DIC_vec,
+               num_eff_param=num_eff_param,
                P=c(PR_m1,1-PR_m1),
                winner=1:2==winner)
 }
@@ -49,25 +51,34 @@ evaluate_game <- function(m){
 #' plot(t_obj)
 #' }
 #' @export
-tournament <- function(...,formula=NULL,data=NULL) {
+tournament <- function(formula=NULL,data=NULL,c_param=NULL,...) {
     args <- list(...)
     error_msg <- 'Please provide either formula and data (name arguments explicitly) or four model objects of types gplm, gplm0, plm and plm0.'
-    if(is.null(formula) | is.null(data)){
+    if(!inherits(formula,'formula') | !is.data.frame(data) | (!is.numeric(c_param) & length(c_param)==1)){
+        args <- c(list(formula,data,c_param),args)
         if(length(args)!=4){
             stop(error_msg)
         }else{
-            args_class <- sapply(args,class)
+            args_class <- unlist(lapply(args,class))
             if(!all(sort(args_class)==c('gplm','gplm0','plm','plm0'))){
                 stop(error_msg)
             }else{
                 names(args) <- args_class
+                args <- args[order(names(args))]
+                if(length(unique(lapply(args,function(x) x$data)))!=1){
+                    stop('The four models added have to be fit on the same data set')
+                }
+                c_param <- unique(lapply(args,function(x) x$c_param))
+                if(length(c_param)!=1){
+                    stop('The four models added have to be fit either all with the same stage of zero discharge (c), or all with unknown c')
+                }
             }
         }
     }else{
-        args$gplm <- gplm(formula, data)
-        args$gplm0 <- gplm0(formula, data)
-        args$plm <- plm(formula, data)
-        args$plm0 <- plm0(formula, data)
+        args$gplm <- gplm(formula, data, c_param=c_param)
+        args$gplm0 <- gplm0(formula, data, c_param=c_param)
+        args$plm <- plm(formula, data, c_param=c_param)
+        args$plm0 <- plm0(formula, data, c_param=c_param)
     }
     round1<- list(list(args$gplm,args$gplm0),list(args$plm,args$plm0))
     round1_res <- lapply(1:length(round1),function(i){
@@ -77,22 +88,12 @@ tournament <- function(...,formula=NULL,data=NULL) {
                   })
     round1_res <- do.call(rbind,round1_res)
     round1_winners <- round1_res$model[round1_res$winner]
-    cat("######## ROUND 1 ########\n")
-    print(round1_res, row.names=FALSE)
-    cat("\n")
-    cat(paste("The winners of round 1 are",round1_winners[1],"and",round1_winners[1],sep = " "))
-    cat("\n\n")
 
     round2 <- lapply(1:length(round1),function(i){
         round1[[i]][[which(round1_res$winner[round1_res$game==i])]]
     })
     round2_res <- cbind(data.frame(round=2,game=3),evaluate_game(round2))
     round2_winner <- round2_res$model[round2_res$winner]
-
-    cat("######## ROUND 2 ########\n")
-    print(round2_res, row.names=FALSE)
-    cat("\n")
-    cat(paste0("The overall winner is ",round2_winner,"!",sep = " "))
     out_obj <- list()
     attr(out_obj, "class") <- "tournament"
     out_obj$contestants <- args[!(names(args) %in% c('formula','data'))]
