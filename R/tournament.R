03383bda17a1dcd02750b40215e1093a1c0db665
Sölvi Rögnvaldsson <sor16@hi.is>
2021-05-25 17:29:26 +0000

    removed explicit attribute assignment to winner

--- R/tournament.R
+++ R/tournament.R
@@ -74,6 +74,7 @@ tournament <- function(formula=NULL,data=NULL,...) {
             }
         }
     }else{
+        args <- list()
         args$gplm <- gplm(formula, data, ...)
         args$gplm0 <- gplm0(formula, data, ...)
         args$plm <- plm(formula, data, ...)
@@ -97,7 +98,6 @@ tournament <- function(formula=NULL,data=NULL,...) {
     attr(out_obj, "class") <- "tournament"
     out_obj$contestants <- args[!(names(args) %in% c('formula','data'))]
     out_obj$winner <- round2[[which(round2_res$winner)]]
-    attr(out_obj$winner, "class") <- class(round2[[which(round2_res$winner)]])                                    
     out_obj$summary <- rbind(round1_res,round2_res)
     return(out_obj)
 }
