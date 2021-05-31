b275a2a8027df10ad6ee3026729f6de9b976f26c
Sölvi Rögnvaldsson <sor16@hi.is>
2021-05-31 22:48:07 +0000

    data update. Adding 4 rivers from Sweden to package

--- R/data.R
+++ R/data.R
@@ -9,3 +9,51 @@
 #' }
 #' @source Icelandic Meteorological Office. \url{http://www.vedur.is}
 "V316_river"
+
+#' Bunnerviken river in Sweden
+#'
+#' Paired stage (W) and discharge measurements (Q) from Bunnerviken river in Sweden
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{W}{Measurements of river stage}
+#'  \item{Q}{Measurements of river discharge}
+#' }
+#' @source Swedish Meteorological and Hydrological Institute \url{http://www.smhi.se}
+"bunnerviken"
+
+#' Flotemarken river in Sweden
+#'
+#' Paired stage (W) and discharge measurements (Q) from Flotemarken river in Sweden
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{W}{Measurements of river stage}
+#'  \item{Q}{Measurements of river discharge}
+#' }
+#' @source Swedish Meteorological and Hydrological Institute \url{http://www.smhi.se}
+"flotemarken"
+
+#' Lisjobacken river in Sweden
+#'
+#' Paired stage (W) and discharge measurements (Q) from Lisjobacken river in Sweden
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{W}{Measurements of river stage}
+#'  \item{Q}{Measurements of river discharge}
+#' }
+#' @source Swedish Meteorological and Hydrological Institute \url{http://www.smhi.se}
+"lisjobacken"
+
+#' Halla river in Sweden
+#'
+#' Paired stage (W) and discharge measurements (Q) from Halla river in Sweden
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{W}{Measurements of river stage}
+#'  \item{Q}{Measurements of river discharge}
+#' }
+#' @source Swedish Meteorological and Hydrological Institute \url{http://www.smhi.se}
+"halla"
--- data-raw/V316_river.R
+++
@@ -1,3 +0,0 @@
-V316_river <- read.table('data-raw/V316_river.tsv',header=T)
-
-usethis::use_data(V316_river, overwrite = TRUE)
