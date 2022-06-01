298b0609702a4a317fc43598f1050082257d06bc
RafaelVias <rafaelvias@protonmail.com>
2022-06-01 21:32:41 +0000

    Add two new datasets, Kallstorp and Melby.

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
-#' Norn stream gauging station in Sweden
+#' Norn gauging station in Sweden
 #'
-#' Data on discharge and stage from Norn stream gauging station in Sweden.
+#' Data on discharge and stage from Norn gauging station in Sweden.
 #'
 #' @format A data frame with columns:
 #' \describe{
@@ -10,9 +10,9 @@
 #' @source Swedish Meteorological and Hydrological Institute.
 "norn"
 
-#' Krokfors stream gauging station in Sweden
+#' Krokfors gauging station in Sweden
 #'
-#' Data on discharge and stage from Krokfors stream gauging station in Sweden.
+#' Data on discharge and stage from Krokfors gauging station in Sweden.
 #'
 #' @format A data frame with columns:
 #' \describe{
@@ -22,9 +22,9 @@
 #' @source Swedish Meteorological and Hydrological Institute.
 "krokfors"
 
-#' Spanga stream gauging station in Sweden
+#' Spanga gauging station in Sweden
 #'
-#' Data on discharge and stage from Spanga stream gauging station in Sweden.
+#' Data on discharge and stage from Spanga gauging station in Sweden.
 #'
 #' @format A data frame with columns:
 #' \describe{
@@ -34,9 +34,9 @@
 #' @source Swedish Meteorological and Hydrological Institute.
 "spanga"
 
-#' Skogsliden stream gauging station in Sweden
+#' Skogsliden gauging station in Sweden
 #'
-#' Data on discharge and stage from Skogsliden stream gauging station in Sweden
+#' Data on discharge and stage from Skogsliden gauging station in Sweden
 #'
 #' @format A data frame with columns:
 #' \describe{
@@ -45,3 +45,27 @@
 #' }
 #' @source Swedish Meteorological and Hydrological Institute.
 "skogsliden"
+
+#' Kallstorp gauging station in Sweden
+#'
+#' Data on discharge and stage from Kallstorp gauging station in Sweden
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{W}{Measurements of water stage in meters}
+#'  \item{Q}{Measurements of water discharge in cubic meters per second}
+#' }
+#' @source Swedish Meteorological and Hydrological Institute.
+"kallstorp"
+
+#' Melby gauging station in Sweden
+#'
+#' Data on discharge and stage from Melby gauging station in Sweden
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{W}{Measurements of water stage in meters}
+#'  \item{Q}{Measurements of water discharge in cubic meters per second}
+#' }
+#' @source Swedish Meteorological and Hydrological Institute.
+"melby"
--- data-raw/add_data.R
+++ data-raw/add_data.R
@@ -1,10 +1,14 @@
 
 norn <- read.csv('data-raw/norn.csv',header=T)
-krokfors<- read.csv('data-raw/krokfors.csv',header=T)
+krokfors <- read.csv('data-raw/krokfors.csv',header=T)
 spanga <- read.csv('data-raw/spanga.csv',header=T)
 skogsliden <- read.csv('data-raw/skogsliden.csv',header=T)
+kallstorp <- read.csv('data-raw/kallstorp.csv',header=T)
+melby <- read.csv('data-raw/melby.csv',header=T)
 
 usethis::use_data(norn, overwrite = TRUE)
 usethis::use_data(krokfors, overwrite = TRUE)
 usethis::use_data(spanga, overwrite = TRUE)
 usethis::use_data(skogsliden, overwrite = TRUE)
+usethis::use_data(kallstorp, overwrite = TRUE)
+usethis::use_data(melby, overwrite = TRUE)
