eb4a44aa6ef15686dc4621f4432083ff6996210f
RafaelVias <rafaelvias@protonmail.com>
2022-06-09 11:02:43 +0000

    adding 3 new tests for tournament_methods

--- tests/testthat/test-tournament_methods.R
+++ tests/testthat/test-tournament_methods.R
@@ -1,1 +1,7 @@
 context('Tournament Methods')
+
+test_that("tournament plots can handle different inputs", {
+    expect_error(plot(t_obj,type=NULL))
+    expect_error(plot(t_obj,type=1))
+    expect_error(autoplot(t_obj,type='tournament_results'))
+})
