fc2c4dd2739a6fa2221ba2adf81e329a7e06bc79
RafaelVias <rafaelvias@protonmail.com>
2022-06-23 17:29:29 +0000

    1. remove plot tests from tournament_methods tests 2. build_site

--- tests/testthat/test-tournament_methods.R
+++ tests/testthat/test-tournament_methods.R
@@ -1,7 +1,5 @@
 context('Tournament Methods')
 
 test_that("tournament plots can handle different inputs", {
-    expect_error(plot(t_obj,type=NULL))
-    expect_error(plot(t_obj,type=1))
     expect_error(autoplot(t_obj,type='tournament_results'))
 })
